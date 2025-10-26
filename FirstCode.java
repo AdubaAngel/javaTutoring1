f7aa7e1d77fdc9fe1ecb49b33b3e1edcc545ed4d
aduba <adubaangel7@gmail.com>
2025-10-26 17:28:52 -0300

    Print Statements studied in this lesson

---
+++ FirstCode.java
@@ -0,0 +1,12 @@
+
+public class FirstCode {
+    public static void main(String[] args) {
+        System.out.println("Java");
+        System.out.println("Have a good day!");
+        System.out.println("Learning java is fun");
+        System.out.print("Have a good day! ");
+        System.out.print("Learning java is fun");
+        
+    }
+    
+}
